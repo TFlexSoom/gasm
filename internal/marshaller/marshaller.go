d788cecfd4cdd09ba4ea2b84f8325136d52a4705
Tristan Hilbert (aka TFlexSoom) <tchilbert@live.com>
2023-10-02 10:48:07 -0700

    Finish Parser

--- internal/marshaller/marshaller.go
+++ internal/marshaller/marshaller.go
@@ -1,1 +1,44 @@
 package marshaller
+
+import (
+	"errors"
+	"fmt"
+)
+
+const zeroByte = byte(0)
+
+var hexTranslator = map[rune]byte{
+	'F': 15,
+	'E': 14,
+	'D': 13,
+	'C': 12,
+	'B': 11,
+	'A': 10,
+	'9': 9,
+	'8': 8,
+	'7': 7,
+	'6': 6,
+	'5': 5,
+	'4': 4,
+	'3': 3,
+	'2': 2,
+	'1': 1,
+	'0': 0,
+}
+
+func MarshallHexByte(value string) error {
+	res := make([]byte, ((len(value)-2)/2)+1)
+	append := func(idx int, val byte) {
+		res[idx] = (res[idx] << 4) + val
+	}
+
+	for i, r := range value[2:] {
+		val := hexTranslator[r]
+		if r != '0' && val == zeroByte {
+			return errors.New(fmt.Sprintf("Unknown Hex Char %v", r))
+		}
+		append(i/2, val)
+	}
+
+	return nil
+}
--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -12,18 +12,17 @@ import (
 // // Lexer
 func getAsmLexer() (*lexer.StatefulDefinition, error) {
 	return lexer.NewSimple([]lexer.SimpleRule{
-		{Name: "WHITESPACE", Pattern: `[ \t]+`},
-		{Name: "EOL", Pattern: `\r?\n`},
-		{Name: "COMMENT_OP", Pattern: `;`},
-		{Name: "SECTION_PREFIX", Pattern: `.`},
+		{Name: "COMMENT", Pattern: `;[~\n]*\n`},
+		{Name: "SECTION_PREFIX", Pattern: `[.]`},
 		{Name: "LABEL_OP", Pattern: `:`},
 		{Name: "ARG_SEP", Pattern: `,`},
-		{Name: "HEX", Pattern: `0x[\d]+`},
-		{Name: "DECIMAL", Pattern: `[\d]+\.[\d]*`},
-		{Name: "INTEGER", Pattern: `[\d]+`},
+		{Name: "HEX", Pattern: `0x[\dA-Fa-f]+`},
+		{Name: "DECIMAL", Pattern: `-?[\d]+\.[\d]*`},
+		{Name: "INTEGER", Pattern: `-?[\d]+`},
 		{Name: "QUOTED_VAL", Pattern: `"[~"]*"`},
 		{Name: "BOOL_VALUE", Pattern: `true|false`},
-		{Name: "IDENTIFIER", Pattern: `[a-zA-Z][a-zA-Z\d]*`},
+		{Name: "IDENTIFIER", Pattern: `[_a-zA-Z][_a-zA-Z\d]*`},
+		{Name: "WHITESPACE", Pattern: `[ \t\r\n]+`},
 	})
 }
 
@@ -36,6 +35,18 @@ func GetAsmParser() (*participle.Parser[Assembly], error) {
 	return participle.Build[Assembly](
 		participle.Lexer(lexer),
 		participle.UseLookahead(1),
+		participle.Union[Term](
+			Comment{},
+			Section{},
+			Instruction{},
+		),
+		participle.Union[Argument](
+			HexArg{},
+			Decimal{},
+			Integer{},
+			String{},
+			Identifier{},
+		),
 	)
 }
 
@@ -43,4 +54,106 @@ func GetAsmParser() (*participle.Parser[Assembly], error) {
 
 type Assembly struct {
 	Pos lexer.Position
+
+	Terms []Term `@@*`
+}
+
+type Term interface {
+	Term()
+	Pos() lexer.Position
+}
+
+type Comment struct {
+	Position lexer.Position
+
+	Comment string `COMMENT+`
+}
+
+func (commentBlock Comment) Term() {}
+func (commentBlock Comment) Pos() lexer.Position {
+	return commentBlock.Position
+}
+
+type Section struct {
+	Position lexer.Position
+
+	Name string `section SECTION_PREFIX @IDENTIFIER`
+}
+
+func (section Section) Term() {}
+func (section Section) Pos() lexer.Position {
+	return section.Position
+}
+
+type Instruction struct {
+	Position lexer.Position
+
+	Label string   `@IDENTIFIER LABEL_OP`
+	Name  string   `@IDENTIFIER`
+	Args  Argument `(@@ ARG_SEP)* @@?`
+}
+
+func (instruction Instruction) Term() {}
+func (instruction Instruction) Pos() lexer.Position {
+	return instruction.Position
+}
+
+type Argument interface {
+	Argument()
+	Pos() lexer.Position
+}
+
+type HexArg struct {
+	Position lexer.Position
+
+	Value string `@HEX`
+}
+
+func (hexArg HexArg) Argument() {}
+func (hexArg HexArg) Pos() lexer.Position {
+	return hexArg.Position
+}
+
+type Decimal struct {
+	Position lexer.Position
+
+	Value string `@DECIMAL`
+}
+
+func (decimal Decimal) Argument() {}
+func (decimal Decimal) Pos() lexer.Position {
+	return decimal.Position
+}
+
+type Integer struct {
+	Position lexer.Position
+
+	Value string `@INTEGER`
+}
+
+func (integer Integer) Argument() {}
+func (integer Integer) Pos() lexer.Position {
+	return integer.Position
+}
+
+type String struct {
+	Posotion lexer.Position
+
+	Value string `@QUOTED_VAL`
+}
+
+func (str String) Argument() {}
+func (str String) Pos() lexer.Position {
+	return str.Posotion
+}
+
+type Identifier struct {
+	Position lexer.Position
+
+	Value string `@IDENTIFIER`
+}
+
+func (identifier Identifier) Argument() {}
+func (identifier Identifier) Pos() lexer.Position {
+	return identifier.Position
 }
