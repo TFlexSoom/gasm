66cb1d81ee19caae2b46f31c00024cc7fb9eef8f
Tristan Hilbert (aka TFlexSoom) <tchilbert@live.com>
2023-10-03 17:26:49 -0700

    BREAKING COMMIT: Start working on linker and brainstorm formats

--- go.mod
+++ go.mod
@@ -9,4 +9,5 @@ require (
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/urfave/cli/v2 v2.25.7 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+	golang.org/x/exp v0.0.0-20230905200255-921286631fa9
 )
--- internal/linker/linking.go
+++ internal/linker/linking.go
@@ -1,1 +1,68 @@
 package linker
+
+import "golang.org/x/exp/maps"
+
+// https://en.wikipedia.org/wiki/Comparison_of_executable_file_formats
+// 1. Support PE
+// 2. Support ELF
+// 3. Support PE32
+// 4. Support Mach-O
+
+type ExternRef string
+type LibraryName string
+type Path string
+
+type BinaryFile interface {
+}
+
+type symbolTableImpl struct {
+	table        map[ExternRef]uintptr
+	tableReverse map[uintptr]ExternRef
+	libraries    map[LibraryName]Path
+	formats      map[string]func([]byte) BinaryFile
+}
+
+type SymbolTable interface {
+	GetSymbols() []ExternRef
+	ReadLibraries([]Path) bool
+	IsDefined() bool
+	Formats() []string
+}
+
+func (symTable symbolTableImpl) addSymbol(ref ExternRef, address uintptr) {
+	symTable.table[ref] = address
+	symTable.tableReverse[address] = ref
+}
+
+func (symTable symbolTableImpl) GetSymbols() []ExternRef {
+	return maps.Keys(symTable.table)
+}
+
+func (symTable symbolTableImpl) ReadLibraries(paths []Path) bool {
+	for _, path := range paths {
+		// 1. Read File At Path into Respective Executable Struct
+		// 2. Read External Symbols Required for the File From Header
+		// 3. Read Defined Symbols in the file for byte addresses, Read all into Map
+		//  so we don't have to return back to the structure
+	}
+
+	return symTable.IsDefined()
+}
+
+func (symTable symbolTableImpl) IsDefined() bool {
+	return len(symTable.table) == len(symTable.tableReverse)
+}
+
+func (symTable symbolTableImpl) Formats() []string {
+	return maps.Keys(symTable.formats)
+}
+
+func getFormatReaders(formats []string) map[string](func([]byte) BinaryFile) {
+	return make(map[string]func([]byte) BinaryFile, 0)
+}
+
+func NewTable(formats []string) SymbolTable {
+	return symbolTableImpl{
+		formats: getFormatReaders(formats),
+	}
+}
